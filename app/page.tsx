a84a902f8c3237809386398cfa6f83dc5fb592c0
David Ojo <dieavid01@gmail.com>
2026-08-18 01:22:28 -0500

    update Home component structure and styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+"use client";
+
+import { MultiStepForm } from "./components/MultiStepForm";
+
 export default function Home() {
   return (
     <div className="">
-      <main className="">
-      
+      <main className="min-h-screen">
+        <MultiStepForm />
       </main>
     </div>
   );
