5cf71bd479e41a4aaf78047e6caeb905e771e554
David Ojo <dieavid01@gmail.com>
2026-09-09 11:12:06 -0500

    refactor FormStep component to improve button structure and enhance accessibility

--- app/components/FormStep.tsx
+++ app/components/FormStep.tsx
@@ -4,12 +4,25 @@ interface FormStepProps {
   index: number;
   stepInfo: StepInfo;
   currentStep: number;
+  jumpToStep: (step: number) => void;
+  hasFinishedForm: boolean;
 }
-export const FormStep = ({ index, stepInfo, currentStep }: FormStepProps) => {
+export const FormStep = ({
+  index,
+  stepInfo,
+  currentStep,
+  jumpToStep,
+  hasFinishedForm,
+}: FormStepProps) => {
   return (
-    <div className="">
+    <button
+      className="md:flex md:items-center md:gap-4 md:text-left md:w-full md:px-6 md:py-3 "
+      type="button"
+      disabled={currentStep < index || hasFinishedForm}
+      onClick={() => jumpToStep(index)}
+    >
       <span
-        className={`flex items-center justify-center w-10 h-10 rounded-full font-medium bg-primary-blue-lighter  ${
+        className={`flex items-center justify-center w-10 h-10 rounded-full font-medium bg-primary-blue-lighter xl:h-12 xl:w-12 xl:font-bold xl:text-xl ${
           currentStep === index
             ? "bg-primary-blue text-primary-blue"
             : " bg-transparent border border-neutral-white text-neutral-white"
@@ -17,8 +30,14 @@ export const FormStep = ({ index, stepInfo, currentStep }: FormStepProps) => {
       >
         {index}
       </span>
-      <p className="hidden md-block">{stepInfo.name}</p>
-      <p className="hidden md-block">{stepInfo.description}</p>
-    </div>
+      <div className="hidden md:block md:uppercase">
+        <p className="text-neutral-grey text-sm xl:text-base xl:font-medium">
+          {stepInfo.name}
+        </p>
+        <p className="text-neutral-white tracking-wider font-medium xl:text-lg xl:font-bold">
+          {stepInfo.description}
+        </p>
+      </div>
+    </button>
   );
 };
