fd1f28972d8c0e1d39ab9d36fb02929dc7fb43e6
David Ojo <dieavid01@gmail.com>
2026-09-09 11:12:31 -0500

    refactor AddonsStep component to utilize react-hook-form for form handling and improve UI structure

--- app/components/AddonsStep.tsx
+++ app/components/AddonsStep.tsx
@@ -1,43 +1,38 @@
 import Image from "next/image";
-import { Addon, FormState } from "../types/types";
+import { FormState } from "../types/types";
 import { ADDONS_LIST } from "../utils/constants";
+import { useFormContext } from "react-hook-form";
+import { Heading } from "./Heading";
 
-interface AddonsStepProps {
-  formData: FormState;
-  handleSelectedAddons: (addon: Addon) => void;
-}
+export const AddonsStep = () => {
+  const { register, getValues } = useFormContext<FormState>();
+  const frequency = getValues("frequency");
 
-export const AddonsStep = ({
-  formData: { frequency, addons: selectedAddons },
-  handleSelectedAddons,
-}: AddonsStepProps) => {
   return (
     <div>
-      <h2 className="text-2xl font-bold text-primary-blue"> Pick add-ons</h2>
-      <p className="text-neutral-grey font-normal mt-2">
-        Add-ons help enhance your gaming experience.
-      </p>
+      <Heading
+        title="Pick add-ons"
+        description="Add-ons help enhance your gaming experience."
+      />
 
-      <div className="space-y-3 mt-6">
+      <div className="space-y-3 mt-6 xl:space-y-5">
         {ADDONS_LIST.map((addon) => (
           <label
             key={addon.name}
             htmlFor={addon.name}
-            className="text-sm cursor-pointer peer block"
+            className="cursor-pointer peer block"
           >
-            <div className="border rounded-lg p-4 transition-colors border-neutral-grey has-checked:border-primary-purple has-checked:bg-neutral-blue-lighter flex gap-4 items-center">
+            <div className="border rounded-lg p-4 transition-colors border-neutral-grey has-checked:border-primary-purple has-checked:bg-neutral-blue-lighter flex gap-4 items-center hover:border-primary-purple xl:px-8 xl:py-6 xl:gap-8">
               <div>
                 <input
                   type="checkbox"
+                  {...register("addons")}
                   id={addon.name}
-                  name={addon.name}
-                  onChange={() => handleSelectedAddons(addon)}
                   value={addon.name}
-                  checked={selectedAddons.includes(addon.name)}
                   className="peer sr-only"
                 />
 
-                <div className="border border-neutral-grey w-5 h-5 rounded-sm flex justify-center items-center peer-checked:bg-primary-purple">
+                <div className="border border-neutral-grey w-5 h-5 rounded-sm flex justify-center items-center peer-checked:bg-primary-purple lg:w-6 lg:h-6">
                   <Image
                     src={"/images/icon-checkmark.svg"}
                     alt="checkmark icon"
@@ -49,13 +44,19 @@ export const AddonsStep = ({
               </div>
               <div className="w-full flex justify-between items-center">
                 <div>
-                  <h4 className="text-primary-blue font-medium">
+                  <h4 className="text-primary-blue font-medium text-sm lg:text-lg">
                     {addon.name}
                   </h4>
-                  <p className="text-neutral-grey">{addon.description}</p>
+                  <p className="text-neutral-grey text-xs lg:text-sm">
+                    {addon.description}
+                  </p>
                 </div>
-                <p className="text-sm text-primary-purple">
-                  {addon.pricing[frequency].price}
+                <p className="text-sm text-primary-purple lg:text-base">
+                  {frequency === "monthly" ? (
+                    <>+${addon.pricing.monthly.price}/mo</>
+                  ) : (
+                    <>+${addon.pricing.yearly.price}/yr</>
+                  )}
                 </p>
               </div>
             </div>
